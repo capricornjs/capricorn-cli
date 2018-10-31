f48b793890d7c6c6bc2dec24471841f6af4a07f7
yangxi <599321378@qq.com>
2018-10-31 12:31:43 +0800

    feat: 优化release命令

--- src/commands/release.js
+++ src/commands/release.js
@@ -1,12 +1,17 @@
 const { log, renderAscii } = require('../core/util')
 const { addModule } = require('../server/release')
 const Spinner = require('../core/spinner')
+const compose = require('koa-compose')
 const path = require('path')
 const fs = require('fs')
 
 const releaseHandler = {
 	init () {
-		this.getInfo()
+		compose([
+			this.getInfo.bind(this),
+			this.getDeclare.bind(this),
+			this.submit.bind(this)
+		])()
 	},
 	
 	moduleInfo: {
@@ -16,15 +21,24 @@ const releaseHandler = {
 		configDeclare: ''
 	},
 	
-	getInfo () {
+	getInfo (ctx, next) {
 		const packagePath = './package.json'
 		fs.readFile(packagePath, { encoding: 'utf8' }, (err, data) => {
 			if (err) throw err
 			let packageObj = JSON.parse(data.toString())
-			this.moduleInfo.name = __filename
+			this.moduleInfo.name = path.basename('./')
 			this.moduleInfo.description = packageObj.description
 			this.moduleInfo.packageName = packageObj.name
-			console.log(this.moduleInfo)
+			next()
+		})
+	},
+	
+	getDeclare (ctx, next) {
+		const declarePath = './module.config.json'
+		fs.readFile(declarePath, { encoding: 'utf8' }, (err, data) => {
+			if (err) throw err
+			this.moduleInfo.configDeclare = JSON.stringify(data)
+			next()
 		})
 	},
 	
@@ -32,7 +46,7 @@ const releaseHandler = {
 	submit () {
 		const spinner = new Spinner('提交module...')
 		spinner.start()
-		addModule().then(() => {
+		addModule(this.moduleInfo).then(() => {
 			log.success('提交module成功.')
 			renderAscii()
 			spinner.stop()
--- src/server/release.js
+++ src/server/release.js
@@ -1,8 +1,13 @@
 const fetch = require('node-fetch')
 
-const addModule = () => {
-	const api = 'http://devm.meitun.com:3000/module/list'
-	return fetch(api, { method: 'GET' }).then(response => {
+const addModule = (body) => {
+	const api = 'http://localhost:3000/module/add'
+
+	return fetch(api, {
+		method: 'POST',
+		body: JSON.stringify(body),
+		headers: { 'Content-Type': 'application/json' }
+	}).then(response => {
 		const { status } = response
 		return status !== 200 ? Promise.reject(new Error('同步module失败')) : response.json()
 	})
